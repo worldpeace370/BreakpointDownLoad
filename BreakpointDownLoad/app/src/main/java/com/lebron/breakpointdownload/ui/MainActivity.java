b0e996a3704c956591cf94fb307d7baccbffc76f
wuxiangkun <worldpeace370@163.com>
2016-08-04 13:49:21 +0800

    启动Activity

--- BreakpointDownLoad/app/src/main/java/com/lebron/breakpointdownload/ui/MainActivity.java
+++ BreakpointDownLoad/app/src/main/java/com/lebron/breakpointdownload/ui/MainActivity.java
@@ -1,13 +1,17 @@
-package com.lebron.breakpointdownload;
+package com.lebron.breakpointdownload.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import com.lebron.breakpointdownload.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,27 +30,20 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        ButterKnife.inject(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    @OnClick({R.id.single, R.id.multith})
+    public void onClick(View view){
+        int id = view.getId();
+        switch (id){
+            case R.id.single:
+                startActivity(new Intent(this, SingleDownload.class));
+                break;
+            case R.id.multith:
+                startActivity(new Intent(this, MultiDownLoad.class));
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
